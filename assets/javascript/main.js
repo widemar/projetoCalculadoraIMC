2f84d21f467e23d2a15a0bf863948be60abc94f8
widemar <widemarinternship@gmail.com>
2020-04-06 15:51:33 -0300

    projetoAtualizado v0.3

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -7,29 +7,43 @@ function escopoGlobal() {
     let altura = Number(document.querySelector("#altura").value);
     let peso = Number(document.querySelector("#peso").value);
     let resultado = document.querySelector("#resultado");
-
+    
     let IMC = peso / (altura * altura);
-  
-    if (IMC < 18.5) {
+    
+    console.log("Peso: ", peso);
+    console.log("Altura: ", altura);
+   
+   
+
+    if (IMC > 0 && IMC < 18.5) {
       document.getElementById("resultado").style.backgroundColor = "#ffd700";
       return resultado.innerHTML = "Abaixo do peso";
-    } else if (IMC <= 24.9) {
+    } else if ((peso == 0 || Number.isNaN(peso)) && (altura == 0 || Number.isNaN(altura))) {
+      document.getElementById("resultado").style.backgroundColor = "#ffd700";
+      return resultado.innerHTML = "Peso e altura inválidos";
+    } else if (peso == 0 || Number.isNaN(peso)) {
+      document.getElementById("resultado").style.backgroundColor = "#ffd700";
+      return resultado.innerHTML = "Peso inválido";
+    } else if (altura == 0 || Number.isNaN(altura)) {
+      document.getElementById("resultado").style.backgroundColor = "#ffd700";
+      return resultado.innerHTML = "Altura inválida";
+    } else if (IMC >= 18.5 && IMC <= 24.9) {
       document.getElementById("resultado").style.backgroundColor = "#00FF7F";
       return resultado.innerHTML = "Peso normal";
-    } else if (IMC <= 29.9) {
+    } else if (IMC >= 25 && IMC <= 29.9) {
       document.getElementById("resultado").style.backgroundColor = "#ffd700";
       return resultado.innerHTML = "Sobrepeso";
-    } else if (IMC <= 34.9) {
+    } else if (IMC >= 30 && IMC <= 34.9) {
       document.getElementById("resultado").style.backgroundColor = "#ff4040";
       return resultado.innerHTML = "Obesidade Grau 1";
-    } else if (IMC <= 39.9) {
+    } else if (IMC >= 35 && IMC <= 39.9) {
       document.getElementById("resultado").style.backgroundColor = "#ff0000";
       return resultado.innerHTML = "Obesidade Grau 2";
-    } else {
+    } else if (IMC > 40) {
       document.getElementById("resultado").style.backgroundColor = "#cc0000";
       return resultado.innerHTML = "Obesidade Grau 3";
-
     }
+
 
 
   }
